03b3347d7f450bb67a38b761c92f289bd32bf5c0
Eduardo Jose Schmitt <eduschmitt@hotmail.com>
2019-06-14 07:20:26 -0300

    Editing file in the branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Tenho que estudar muito mais mesmo!</p>
+          <p>Tenho que estudar muito mais! Kubernets também!</p>
           <a
             className="App-link"
             href="https://reactjs.org"
